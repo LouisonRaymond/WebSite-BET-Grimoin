1bcee896c6f67a4b0195e57e52c8a3f68519ba8b
Baptiste Raymond <11996931+belisarus@users.noreply.github.com>
2023-10-27 00:51:48 +0200

    add expertises page

--- src/backend/routes/expertises.js
+++ src/backend/routes/expertises.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const { getExpertises } = require('../services/expertises-service');
 const { getTranslations } = require('../services/translation-service');
-const router = express.Router({mergeParams: true});
+const router = express.Router({ mergeParams: true });
 
 /* GET expertises page. */
 router.get('/', async function (req, res, next) {
   const locale = req.lang;
+  const expertises = await getExpertises(locale);
   const translations = await getTranslations(['global', 'expertises'], locale);
 
   const model = {
     locale,
-    translations
+    translations,
+    expertises
   };
   res.render('expertises', { model });
 });
